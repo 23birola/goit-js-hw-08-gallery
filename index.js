fff4a765ddd860b6c5c9eddff53b0ad2860d7ca9
Olga <23birola@gmail.com>
2021-04-14 21:11:57 +0300

    additional task

--- index.js
+++ index.js
@@ -20,6 +20,7 @@ function createGalleryMarkup(items) {
 
 const galleryList = document.querySelector("ul.js-gallery");
 const modal = document.querySelector('.js-lightbox');
+const overlay = document.querySelector('.lightbox__overlay');
 const modalImage = document.querySelector('.js-lightbox img');
 const closeBtn = document.querySelector("button[data-action=close-lightbox]");
 
@@ -43,4 +44,53 @@ closeBtn.addEventListener('click', () => {
   }
   return;
 });
+
+overlay.addEventListener('click', (e) => {
+  if (e.target !== e.currentTarget) {
+    return;
+  }
+  if (modal.classList.contains('is-open')) {
+    modal.classList.remove('is-open');
+    modalImage.src = "";
+  }
+  return;
+});
+
+window.addEventListener('keydown', (e) => {
+  if (e.code !== "Escape") {
+    return;
+  }
+  if (modal.classList.contains('is-open')) {
+    modal.classList.remove('is-open');
+    modalImage.src = "";
+  }
+  return;
+});
+
+  let index = 0;
+  for (let i = 0; i < gallery.length; i += 1) {
+    if (gallery[i].original === modalImage.src) {
+      index = i;
+    }
+  }
+ 
+  window.addEventListener('keydown', (e) => {
+    if (e.code === "ArrowRight") {
+      if (index !== gallery.length - 1) {
+        index += 1;
+      } else { index = 0; }
+      modalImage.src = gallery[index].original;
+      return;
+    }
+  })
+   
+  window.addEventListener('keydown', (e) => {
+    if (e.code === "ArrowLeft") {
+      if (index !== 0) {
+        index -= 1;
+      } else { index = gallery.length - 1; }
+      modalImage.src = gallery[index].original;
+      return;
+    }
+  })
 
